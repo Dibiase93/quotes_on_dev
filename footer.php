8f2a158c7339f4324467aca17dafc0c23b2e58fe
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-06-02 17:13:23 -0700

    add footer styling in navigation.scss file with nav style aswell

--- footer.php
+++ footer.php
@@ -10,11 +10,13 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+				<div class="site-info">
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</nav><!-- #site-navigation -->
+					<p>Brought to you by <a href="https://redacademy.com/vancouver/">RED Academy</a></p>	
+				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
--- page-about.php
+++ page-about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The main template file.
+ * The template for displaying all pages.
  *
  * @package QOD_Starter_Theme
  */
@@ -10,54 +10,26 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                    </header><!-- .entry-header -->
 
-			<?php endwhile; ?>
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                        <?php
+                            wp_link_pages( array(
+                                'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+                                'after'  => '</div>',
+                            ) );
+                        ?>
+                    </div><!-- .entry-content -->
+                </article><!-- #post-## -->
 
-			<?php the_posts_navigation(); ?>
 
-		<?php else : ?>
-
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-			<div class="entry-content">
-				<?php the_excerpt(); ?>
-			</div><!-- .entry-content -->
-
-			<div class="entry-meta">
-				<?php the_title('<h2 class="entry-title"> &mdash; ', '</h2>'); ?>
-
-				<?php if($source && $source_url): ?>
-					<span class="source"> 
-						<a href="<?php echo $source_url; ?>">,
-							<?php echo $source; ?>
-						</a>
-					</span>
-
-				<?php elseif($source): ?>
-
-					<span class="source">, <?php echo $source; ?> </span>
-
-				<?php else: ?>
-
-					<span class="source"> </span>
-
-				<?php endif; ?>
-			</div>
-		</article><!-- #post-## -->
-
-		<?php endif; ?>
+			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
